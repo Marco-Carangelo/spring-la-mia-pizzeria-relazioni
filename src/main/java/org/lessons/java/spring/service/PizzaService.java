18700b70d1c39b5be93065f305527f9acf06a8ab
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-09-11 18:28:57 +0200

    add: methods for the cruds

--- src/main/java/org/lessons/java/spring/service/PizzaService.java
+++ src/main/java/org/lessons/java/spring/service/PizzaService.java
@@ -1,6 +1,9 @@
 package org.lessons.java.spring.service;
 
+import java.util.List;
+
 import org.lesson.java.spring.controller.PizzaRepository;
+import org.lesson.java.spring.model.Pizza;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +12,29 @@ public class PizzaService {
 	
 	@Autowired
 	private PizzaRepository repository;
-
+	
+	
+	public List<Pizza> findPizzas(){
+		return repository.findAll();
+	}
+	
+	public List<Pizza> searchPizzas(String name){
+		return repository.findByNomeStartsWith(name);
+	}
+	
+	public Pizza findPizzaById(Integer id){
+		return repository.findById(id).get();
+	}
+	
+	public void createPizza(Pizza pizza){
+		 repository.save(pizza);
+	}
+	
+	public void editPizza(Pizza pizza){
+		 repository.save(pizza);
+	}
+	
+	public void deletePizzaById(Integer id){
+		 repository.deleteById(id);
+	}
 }
