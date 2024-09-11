2b6378b70758d2a27cc624e618817e40cceb15ca
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-09-11 18:46:39 +0200

    add: getters and setters

--- src/main/java/org/lesson/java/spring/model/Offerta.java
+++ src/main/java/org/lesson/java/spring/model/Offerta.java
@@ -36,4 +36,46 @@ public class Offerta {
 	@JoinColumn(name="pizza_id", nullable=false)
 	private Pizza pizza;
 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitoloOfferta() {
+		return titoloOfferta;
+	}
+
+	public void setTitoloOfferta(String titoloOfferta) {
+		this.titoloOfferta = titoloOfferta;
+	}
+
+	public LocalDateTime getInizioOfferta() {
+		return inizioOfferta;
+	}
+
+	public void setInizioOfferta(LocalDateTime inizioOfferta) {
+		this.inizioOfferta = inizioOfferta;
+	}
+
+	public LocalDateTime getFineOfferta() {
+		return fineOfferta;
+	}
+
+	public void setFineOfferta(LocalDateTime fineOfferta) {
+		this.fineOfferta = fineOfferta;
+	}
+
+	public Pizza getPizza() {
+		return pizza;
+	}
+
+	public void setPizza(Pizza pizza) {
+		this.pizza = pizza;
+	}
+	
+	
+
 }
