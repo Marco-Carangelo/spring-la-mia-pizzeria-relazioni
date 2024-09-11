261a2de32c747aabc2113cf41e9a2174f47855cf
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-09-11 18:46:07 +0200

    add: Offerta fields and foreing key

--- src/main/java/org/lesson/java/spring/model/Offerta.java
+++ src/main/java/org/lesson/java/spring/model/Offerta.java
@@ -1,10 +1,18 @@
 package org.lesson.java.spring.model;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "offerte")
@@ -13,5 +21,19 @@ public class Offerta {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@NotNull
+	@NotEmpty
+	private String titoloOfferta;
+	
+	@UpdateTimestamp
+	private LocalDateTime inizioOfferta;
+	
+	@UpdateTimestamp
+	private LocalDateTime fineOfferta;
+	
+	@ManyToOne
+	@JoinColumn(name="pizza_id", nullable=false)
+	private Pizza pizza;
 
 }
