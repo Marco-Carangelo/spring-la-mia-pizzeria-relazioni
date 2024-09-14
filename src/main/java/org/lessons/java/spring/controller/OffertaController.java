96ded034ebee316246e25240a1dd275de530b2b8
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-09-14 14:57:35 +0200

    add: create and store methods for Offerta

--- src/main/java/org/lessons/java/spring/controller/OffertaController.java
+++ src/main/java/org/lessons/java/spring/controller/OffertaController.java
@@ -4,12 +4,19 @@ import java.util.List;
 
 import org.lessons.java.spring.model.Offerta;
 import org.lessons.java.spring.service.OffertaService;
+import org.lessons.java.spring.service.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/offerte")
@@ -17,6 +24,9 @@ public class OffertaController {
 	
 	@Autowired
 	private OffertaService offertaService;
+	
+	@Autowired
+	private PizzaService pizzaService;
 	
 	@GetMapping
 	public String index(Model model) {
@@ -31,10 +41,30 @@ public class OffertaController {
 		return "/offerte/show";
 	}
 	
-	@GetMapping("/create")
-	public String create(Model model) {
-		model.addAttribute("offerta", new Offerta());
+	@GetMapping("/create/{id}")
+	public String create(@PathVariable("id") Integer id ,Model model) {
+		Offerta offerta = new Offerta();
+		offerta.setPizza(pizzaService.findPizzaById(id));
+		model.addAttribute("offerta", offerta);
 		return "/offerte/create";
 	}
 
+	@PostMapping("/create")
+	public String store(
+			@Valid @ModelAttribute("offerta") Offerta formOfferta,
+			BindingResult bindingResult,
+			Model model,
+			RedirectAttributes attributes ){
+		
+		if(bindingResult.hasErrors()) {
+			return "/offerte/create";
+		}
+		
+		
+		offertaService.createOfferta(formOfferta);
+		
+		attributes.addFlashAttribute("message", "L'offerta  " + formOfferta.getTitoloOfferta() + " è stata creata");
+		
+		return "redirect:/offerte";
+	}
 }
