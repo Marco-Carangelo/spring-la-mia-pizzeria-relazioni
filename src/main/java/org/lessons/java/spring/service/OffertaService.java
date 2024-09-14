4166a98319200da964854ada0278d3e320cef770
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-09-14 10:24:05 +0200

    add: new service class for the Offerta model

---
+++ src/main/java/org/lessons/java/spring/service/OffertaService.java
@@ -0,0 +1,25 @@
+package org.lessons.java.spring.service;
+
+import java.util.List;
+
+import org.lessons.java.spring.model.Offerta;
+import org.lessons.java.spring.repository.OffertaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OffertaService {
+	
+	@Autowired
+	private OffertaRepository repository;
+	
+	public List<Offerta> findOfferte(){
+		return repository.findAll();
+	}
+	
+	public Offerta findOffertaById(Integer id){
+		return repository.findById(id).get();
+	}
+	
+	
+}
