ec8ee3855002c46187293a5ab7726856c2ca41f0
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-09-16 10:19:28 +0200

    add: Ingrediente.java model

---
+++ src/main/java/org/lessons/java/spring/model/Ingrediente.java
@@ -0,0 +1,57 @@
+package org.lessons.java.spring.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table( name = "ingredienti")
+public class Ingrediente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@NotNull
+	@NotEmpty
+	@Size(min = 3, max = 40)
+	private String nomeIngrediente;
+	
+	@ManyToMany(mappedBy = "ingredienti")
+	private List<Pizza> pizzas;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNomeIngrediente() {
+		return nomeIngrediente;
+	}
+
+	public void setNomeIngrediente(String nomeIngrediente) {
+		this.nomeIngrediente = nomeIngrediente;
+	}
+
+	public List<Pizza> getPizzas() {
+		return pizzas;
+	}
+
+	public void setPizzas(List<Pizza> pizzas) {
+		this.pizzas = pizzas;
+	}
+	
+	
+
+}
