d033a6faf0c03e40a08c698bbe0455abdee95d2e
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-09-16 12:12:24 +0200

    add: store method for the Ingrediente model

--- src/main/java/org/lessons/java/spring/controller/IngredienteController.java
+++ src/main/java/org/lessons/java/spring/controller/IngredienteController.java
@@ -3,13 +3,18 @@ package org.lessons.java.spring.controller;
 import java.util.List;
 
 import org.lessons.java.spring.model.Ingrediente;
-import org.lessons.java.spring.model.Pizza;
 import org.lessons.java.spring.service.IngredienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/ingredienti")
@@ -32,5 +37,23 @@ public class IngredienteController {
 		model.addAttribute("ingrediente", new Ingrediente());
 		return "/ingredienti/create";
 	}
+	
+	@PostMapping("/create")
+	public String store(
+			@Valid @ModelAttribute("ingrediente") Ingrediente formIngrediente,
+			BindingResult bindingResult,
+			Model model,
+			RedirectAttributes attributes ){
+		
+		if(bindingResult.hasErrors()) {
+			return "/ingredienti/create";
+		}
 
+		
+		ingredienteService.createIngrediente(formIngrediente);
+		
+		attributes.addFlashAttribute("message", "L'ingrediente " + formIngrediente.getNomeIngrediente() + " è stato creato");
+		
+		return "redirect:/ingredienti";
+	}
 }
