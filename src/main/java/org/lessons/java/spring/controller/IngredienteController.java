bf1a75b6355929b229462f81182dee2b4bd72a07
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-09-16 11:09:26 +0200

    add: new controller for the Ingrediente class with Index method

---
+++ src/main/java/org/lessons/java/spring/controller/IngredienteController.java
@@ -0,0 +1,29 @@
+package org.lessons.java.spring.controller;
+
+import java.util.List;
+
+import org.lessons.java.spring.model.Ingrediente;
+import org.lessons.java.spring.service.IngredienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ingredienti")
+public class IngredienteController {
+	
+	@Autowired
+	private IngredienteService ingredienteService;
+	
+	@GetMapping
+	public String index(Model model) {
+		
+		List<Ingrediente> result = ingredienteService.findIngredienti();
+		model.addAttribute("list", result);
+		
+		return  "/ingredienti/index";
+	}
+
+}
