3107f019029f9157c3b6ef6017f0d8c058fc0d41
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-09-16 12:02:47 +0200

    add: create method for Ingrediente

--- src/main/java/org/lessons/java/spring/controller/IngredienteController.java
+++ src/main/java/org/lessons/java/spring/controller/IngredienteController.java
@@ -3,6 +3,7 @@ package org.lessons.java.spring.controller;
 import java.util.List;
 
 import org.lessons.java.spring.model.Ingrediente;
+import org.lessons.java.spring.model.Pizza;
 import org.lessons.java.spring.service.IngredienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -24,6 +25,12 @@ public class IngredienteController {
 		model.addAttribute("list", result);
 		
 		return  "/ingredienti/index";
+	}
+	
+	@GetMapping("/create")
+	public String create(Model model) {
+		model.addAttribute("ingrediente", new Ingrediente());
+		return "/ingredienti/create";
 	}
 
 }
