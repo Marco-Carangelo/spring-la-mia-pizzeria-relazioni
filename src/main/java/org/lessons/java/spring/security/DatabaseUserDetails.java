a3f26ae81598026a7184cbb99eb1d655490d111d
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-09-20 22:23:01 +0200

    add: override of the UserDetails interface method

--- src/main/java/org/lessons/java/spring/security/DatabaseUserDetails.java
+++ src/main/java/org/lessons/java/spring/security/DatabaseUserDetails.java
@@ -34,19 +34,19 @@ public class DatabaseUserDetails implements UserDetails{
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		// TODO Auto-generated method stub
-		return null;
+		return authorities;
 	}
 
 	@Override
 	public String getPassword() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.password;
 	}
 
 	@Override
 	public String getUsername() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.username;
 	}
 	
 	
