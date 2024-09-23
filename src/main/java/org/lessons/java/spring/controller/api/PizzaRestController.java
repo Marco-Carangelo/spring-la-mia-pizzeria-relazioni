361ec2902777fbdbde31cb9a7d6db95a45a1fb09
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-09-23 18:44:37 +0200

    add: store method for the rest api

--- src/main/java/org/lessons/java/spring/controller/api/PizzaRestController.java
+++ src/main/java/org/lessons/java/spring/controller/api/PizzaRestController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +44,13 @@ public class PizzaRestController {
 		return pizza.get();
 	}
 	
+	@PostMapping
+	public Pizza store(@RequestBody Pizza pizza) {
+		Pizza newPizza = pizzaService.createPizza(pizza);
+		
+		return newPizza;
+		
+	}
 	
 			
 			
